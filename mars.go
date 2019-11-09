6022002c1e60751f67bb113741815fef65bba878
Samir Nassar <samir@samirnassar.com>
2019-11-09 18:15:31 +0100

    Lesson 2: Printf with variables

--- mars.go
+++ mars.go
@@ -3,10 +3,6 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Print("My weight on Mars is ")
-  fmt.Print(85.0 * 0.3783)
-  fmt.Print(" kilos, and I would be ")
-  fmt.Print(40 * 365 / 687)
-  fmt.Print(" years old.\n")
+  fmt.Printf("My weight on Mars is %v kilos, ", 85.0 * 0.3783)
+  fmt.Printf("and I would be %v years old.\n", 40 * 365 / 687)
 }
-
