c4854d0f0466ef860136088ca920dcca637203aa
Samir Nassar <samir@samirnassar.com>
2019-11-09 18:13:47 +0100

    Lesson 2: Print

---
+++ mars.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Print("My weight on Mars is ")
+  fmt.Print(85.0 * 0.3783)
+  fmt.Print(" kilos, and I would be ")
+  fmt.Print(40 * 365 / 687)
+  fmt.Print(" years old.\n")
+}
+
